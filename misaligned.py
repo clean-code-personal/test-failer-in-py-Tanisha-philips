0edd4752f6694652c16bb523aeea4a1e56e2cf47
Tanisha-philips <Tanisha.Aggarwal@philips.com>
2024-11-18 16:07:04 +0530

    Update misaligned.py

--- misaligned.py
+++ misaligned.py
@@ -1,7 +1,7 @@
 
 def print_color_map():
     major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
-    minor_colors = ["Blue", "Orange", "Green", "Brown"]
+    minor_colors = ["Blue", "Orange", "Green", "Brown", "Slate"]
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
             print(f'{i * 5 + j} | {major} | {minor}')
